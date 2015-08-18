8dff89c51c43ffd9147971a18d983deab992f7d0
Bill Glass <williamtg087@gmail.com>
2015-08-18 11:48:18 -0400

    updated app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  AWS::S3::Base.establish_connection!(
+ :access_key_id   => ENV['S3_KEY'],
+ :secret_access_key => ENV['S3_SECRET']
+)
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['nbacard'],
-    :access_key_id => ENV['AKIAISEDNIQADVTG24AA'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
